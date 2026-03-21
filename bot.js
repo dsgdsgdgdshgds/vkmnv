e121cdf83414c686d007cc34fb38fce253e0ad8c
dsgdsgdgdshgds <112278874+dsgdsgdgdshgds@users.noreply.github.com>
2026-03-22 00:28:58 +0300

    Update bot.js

--- bot.js
+++ bot.js
@@ -23,6 +23,7 @@ const {
     VoiceConnectionStatus 
 } = require('@discordjs/voice');
 
+
 // ────────────────────────────────────────────────
 // GENEL AYARLAR VE SUNUCU
 // ────────────────────────────────────────────────
